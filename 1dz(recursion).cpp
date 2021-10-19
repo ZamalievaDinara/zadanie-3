f5391956ad12cb93539a7e53adc8e33703546aad
ZamalievaDinara <91804152+ZamalievaDinara@users.noreply.github.com>
2021-10-19 23:10:34 +0300

    Add files via upload

---
+++ 1dz(recursion).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void printDigit(const int n)
+{
+    if (n > 9)
+        printDigit(n / 10);
+
+    cout << n % 10 << " "; // выводит последнюю цифру из n
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите число: ";
+    int N;
+    cin >> N;
+    while (N < 1)
+    {
+        cout << "Введено не натуральное число. " << endl;
+        cout << "Введите число: ";
+        cin >> N;
+    }
+
+    printDigit(N);
+
+    return 0;    
+}
+
