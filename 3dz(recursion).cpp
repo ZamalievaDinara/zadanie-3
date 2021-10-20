8e37bf7b8465eee7475832454f2124ea2f111ff1
ZamalievaDinara <91804152+ZamalievaDinara@users.noreply.github.com>
2021-10-20 22:20:50 +0300

    Add files via upload

---
+++ 3dz(recursion).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+void printFactors(const int n, const int div)
+{
+    if (n == 1)
+        return;     
+    if (n % div == 0)
+    {
+        cout << " * " << div;
+        printFactors(n / div, div);
+    }
+    else 
+        printFactors(n, div + (div == 2 ? 1 : 2));
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите число: ";
+    int N;
+    cin >> N;
+    while (N < 1)
+    {
+        cout << "Введено не натуральное число. " << endl;
+        cout << "Введите число: ";
+        cin >> N;
+    }
+
+    cout << "N = " << N << " = 1";
+    printFactors(N, 2);
+ 
+    return 0;
+}
